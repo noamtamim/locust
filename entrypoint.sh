252abc10a81cb64c997b926489a0a09d2d8d6966
Noam Tamim <noam.tamim@kaltura.com>
2021-07-12 21:08:00 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,18 +5,16 @@
 Usage:
 
 Pass the URL of the locustfile.py file as the first argument. All other arguments go directly to locust.
-DO NOT pass a `-f` argument!
+DO NOT pass a '-f' argument!
 
 ////
 
 set -exu
 
-function download() {
-    # avoid installing curl
-    python -c "import urllib.request as req; req.urlretrieve('$1', '$2')"
-}
 
-download "$1" locustfile.py
+echo My IP: $(curl https://api.ipify.org/)
+
+curl "$1" -o locustfile.py
 
 shift
 
