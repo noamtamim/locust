f5de43fb251ec79af50ede46ee6c7644cc121028
Noam Tamim <noam.tamim@kaltura.com>
2021-07-15 09:30:37 +0300

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -9,6 +9,8 @@ MASTER_CLUSTER="$2"
 MASTER_SERVICE="$3"
 MASTER_PORT="$4"
 
+export AWS_DEFAULT_REGION=$(curl http://169.254.169.254/latest/meta-data/placement/region)
+
 curl "$URL" -o locustfile.py
 
 MASTER_HOST="$(python find_master.py "$MASTER_CLUSTER" "$MASTER_SERVICE")"
